b7b7d055b2f786d7cf01f16623b770d0639ad10b
Fabio Albertin <fabio.albertin@matchesfashion.com>
2017-08-18 18:49:30 +0100

    Finished article tutorial

--- index-v2.js
+++ index-v2.js
@@ -2,18 +2,20 @@ var hydraExpress = require('hydra-express');
 var hydra = hydraExpress.getHydra();
 var config = require('./config.json');
 
-function onRegisterRoutes() {
-  var express = hydraExpress.getExpress();
-  var api = express.Router();
+hydraExpress.init(config, () => {})
+  .then((serviceInfo) => {
+    console.log('serviceInfo', serviceInfo);
 
-  api.get('/', function(req, res) {
-    res.send({
-      msg: `hello from ${hydra.getServiceName()} - ${hydra.getInstanceID()}`
+    hydra.on('message', (message) => {
+      let messageReply = hydra.createUMFMessage({
+        to: message.frm,
+        frm: 'hello:/',
+        bdy: {
+          msg: `hello from ${hydra.getServiceName()} - ${hydra.getInstanceID()}`
+        }
+      });
+      hydra.sendMessage(messageReply);
     });
-  });
-  hydraExpress.registerRoutes({
-    '': api
-  });
-}
-
-hydraExpress.init(config, onRegisterRoutes);
+    return 0;
+  })
+  .catch(err => console.log('err: ', err));
